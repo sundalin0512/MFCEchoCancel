fe96e59f1bdc54959651044886bb18e25437c799
dalin sun <sundalin0512@outlook.com>
2017-06-20 20:09:03 +0800

    q

--- MFCEchoCancel/Resource.h
+++ MFCEchoCancel/Resource.h
@@ -26,6 +26,7 @@
 #define IDC_StaticDelay                 1023
 #define IDC_MinRandonDelay              1024
 #define IDC_MaxRandonDelay              1025
+#define IDC_RADIO1                      1026
 
 // Next default values for new objects
 // 
@@ -33,7 +34,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1026
+#define _APS_NEXT_CONTROL_VALUE         1027
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
